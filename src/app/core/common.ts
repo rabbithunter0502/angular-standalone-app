1a97e85c5597b770e3a4d0e7fdee28ed76b38c96
rabbithunter0502 <43747716+rabbithunter0502@users.noreply.github.com>
2023-10-01 13:58:57 +0700

    chore: add uuid common

--- src/app/core/common.ts
+++ src/app/core/common.ts
@@ -44,3 +44,11 @@ export const clone = (obj: any) => {
   }
   return result;
 };
+
+export const uuid = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
